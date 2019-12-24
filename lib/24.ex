99fd03099d2a9ab0abea055d895ba97da6b1aa00
Stephen Bussey <sb8244@cs.ship.edu>
2019-12-24 01:15:39 -0500

    Day 24 pt2

--- lib/24.ex
+++ lib/24.ex
@@ -1,3 +1,132 @@
+defmodule TwentyFourRecursive do
+  def parse(input) do
+    # make the board bigger than the 200 ticks needed
+    full =
+      for r <- 0..4, c <- 0..4, d <- -150..150 do
+        {{{r, c}, d}, "."}
+      end
+      |> Enum.into(%{})
+
+    TwentyFour.parse(input)
+    |> Enum.map(fn {coord, s} ->
+      {{coord, 0}, s}
+    end)
+    |> Enum.into(full)
+  end
+
+  def next(state) do
+    Enum.reduce(state, %{}, fn
+      {p = {{2, 2}, _}, _}, next ->
+        Map.put(next, p, "?")
+
+      {p, s}, next ->
+        count = neighbor_count(state, p)
+
+        next_s =
+          if s == "#" do
+            if count == 1 do
+              "#"
+            else
+              "."
+            end
+          else
+            if count == 1 || count == 2 do
+              "#"
+            else
+              "."
+            end
+          end
+
+        Map.put(next, p, next_s)
+    end)
+  end
+
+  defp neighbor_count(state, at) do
+    # if at == {{0, 1}, -2} do
+    #   IO.inspect(neighbors(state, at) ++ reg_neighbors(state, at))
+    # end
+
+    (neighbors(state, at) ++ reg_neighbors(state, at))
+    |> Enum.map(fn coor ->
+      Map.get(state, coor, ".")
+    end)
+    |> Enum.filter(fn s -> s == "#" end)
+    |> length()
+  end
+
+  # A
+  defp neighbors(state, {{0, 0}, d}), do: [{{1, 2}, d + 1}, {{2, 1}, d + 1}]
+  # B
+  defp neighbors(state, {{0, 1}, d}), do: [{{1, 2}, d + 1}]
+  # C
+  defp neighbors(state, {{0, 2}, d}), do: [{{1, 2}, d + 1}]
+  # D
+  defp neighbors(state, {{0, 3}, d}), do: [{{1, 2}, d + 1}]
+  # E
+  defp neighbors(state, {{0, 4}, d}), do: [{{1, 2}, d + 1}, {{2, 3}, d + 1}]
+
+  # F
+  defp neighbors(state, {{1, 0}, d}), do: [{{2, 1}, d + 1}]
+  # G
+  defp neighbors(state, {{1, 1}, d}), do: []
+  # H
+  defp neighbors(state, {{1, 2}, d}),
+    do: [{{0, 0}, d - 1}, {{0, 1}, d - 1}, {{0, 2}, d - 1}, {{0, 3}, d - 1}, {{0, 4}, d - 1}]
+
+  # I
+  defp neighbors(state, {{1, 3}, d}), do: []
+  # J
+  defp neighbors(state, {{1, 4}, d}), do: [{{2, 3}, d + 1}]
+
+  # K
+  defp neighbors(state, {{2, 0}, d}), do: [{{2, 1}, d + 1}]
+  # L
+  defp neighbors(state, {{2, 1}, d}),
+    do: [{{0, 0}, d - 1}, {{1, 0}, d - 1}, {{2, 0}, d - 1}, {{3, 0}, d - 1}, {{4, 0}, d - 1}]
+
+  # ?
+  defp neighbors(state, {{2, 2}, d}), do: []
+  # N
+  defp neighbors(state, {{2, 3}, d}),
+    do: [{{0, 4}, d - 1}, {{1, 4}, d - 1}, {{2, 4}, d - 1}, {{3, 4}, d - 1}, {{4, 4}, d - 1}]
+
+  # O
+  defp neighbors(state, {{2, 4}, d}), do: [{{2, 3}, d + 1}]
+
+  # P
+  defp neighbors(state, {{3, 0}, d}), do: [{{2, 1}, d + 1}]
+  # Q
+  defp neighbors(state, {{3, 1}, d}), do: []
+  # R
+  defp neighbors(state, {{3, 2}, d}),
+    do: [{{4, 0}, d - 1}, {{4, 1}, d - 1}, {{4, 2}, d - 1}, {{4, 3}, d - 1}, {{4, 4}, d - 1}]
+
+  # S
+  defp neighbors(state, {{3, 3}, d}), do: []
+  # T
+  defp neighbors(state, {{3, 4}, d}), do: [{{2, 3}, d + 1}]
+
+  # U
+  defp neighbors(state, {{4, 0}, d}), do: [{{2, 1}, d + 1}, {{3, 2}, d + 1}]
+  # V
+  defp neighbors(state, {{4, 1}, d}), do: [{{3, 2}, d + 1}]
+  # W
+  defp neighbors(state, {{4, 2}, d}), do: [{{3, 2}, d + 1}]
+  # X
+  defp neighbors(state, {{4, 3}, d}), do: [{{3, 2}, d + 1}]
+  # Y
+  defp neighbors(state, {{4, 4}, d}), do: [{{2, 3}, d + 1}, {{3, 2}, d + 1}]
+
+  defp reg_neighbors(state, {{r, c}, d}) do
+    [{{r - 1, c}, d}, {{r + 1, c}, d}, {{r, c - 1}, d}, {{r, c + 1}, d}]
+    |> Enum.map(fn coor ->
+      {coor, Map.get(state, coor)}
+    end)
+    |> Enum.reject(fn {_, s} -> is_nil(s) end)
+    |> Enum.map(&elem(&1, 0))
+  end
+end
+
 defmodule TwentyFour do
   def parse(input) do
     String.split(input, "\n", trim: true)
@@ -61,11 +190,11 @@ defmodule TwentyFour do
   end
 
   defp neighbor_count(state, {r, c}) do
-    [{r-1, c}, {r+1, c}, {r, c-1}, {r, c+1}]
+    [{r - 1, c}, {r + 1, c}, {r, c - 1}, {r, c + 1}]
     |> Enum.map(fn coor ->
       Map.get(state, coor)
     end)
-    |> Enum.reject(& is_nil/1)
+    |> Enum.reject(&is_nil/1)
     |> Enum.filter(fn s -> s == "#" end)
     |> length()
   end
--- test/solutions/24_test.exs
+++ test/solutions/24_test.exs
@@ -1,21 +1,20 @@
 defmodule TwentyFourTest do
   use ExUnit.Case
 
-  test "example" do
-    input =
-      """
-      ....#
-      #..#.
-      #..##
-      ..#..
-      #....
-      """
+  # test "example" do
+  #   input =
+  #     """
+  #     ....#
+  #     #..#.
+  #     #..##
+  #     ..#..
+  #     #....
+  #     """
 
-    state = TwentyFour.parse(input)
-    state2 = TwentyFour.next(state)
-
-    # IO.inspect state2
-  end
+  #   state = TwentyFour.parse(input)
+  #   TwentyFour.next(state)
+  #   # |> IO.inspect()
+  # end
 
   test "pt1" do
     input =
@@ -29,5 +28,41 @@ defmodule TwentyFourTest do
 
     state = TwentyFour.parse(input)
     assert TwentyFour.iter_until_repeat(state) |> TwentyFour.sum_bio() == 18350099
+  end
+
+  test "pt2" do
+    input =
+      """
+      ##...
+      #.###
+      .#.#.
+      #....
+      ..###
+      """
+
+    state = TwentyFourRecursive.parse(input)
+
+    s =
+      Enum.reduce((0..199), state, fn _, state ->
+        TwentyFourRecursive.next(state)
+      end)
+
+    # print(s, 5..-5)
+
+    assert s
+    |> Map.values()
+    |> Enum.filter(& &1 == "#")
+    |> length() == 2037
+  end
+
+  def print(state, range) do
+    for dep <- range do
+      IO.write("\n\n\n")
+      for r <- (0..4), c <- (0..4) do
+        IO.write(Map.fetch!(state, {{r, c}, dep}))
+
+        if c == 4, do: IO.write("\n")
+      end
+    end
   end
 end
